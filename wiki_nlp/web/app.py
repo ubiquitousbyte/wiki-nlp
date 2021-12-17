9a7be426ffa88a870d84e70462d44c4c5319498a
nasden <nasdenkov@gmail.com>
2021-12-17 17:01:52 +0100

    add document router to web app

--- wiki_nlp/web/app.py
+++ wiki_nlp/web/app.py
@@ -1,4 +1,18 @@
+from wiki_nlp.web import dependencies
+
+
 if __name__ == '__main__':
-    from fastapi import FastAPI
+    from fastapi import (
+        FastAPI,
+        Depends
+    )
+    from wiki_nlp.web import (
+        document_router,
+    )
 
     app = FastAPI(title='Plagiarism Detector', version='0.1.0')
+
+    app.include_router(
+        document_router.router,
+        prefix='/api/v1'
+    )
